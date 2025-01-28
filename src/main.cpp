7285fd4917d2bdaf872d029bea41262bb5743949
avilkinnick <avilkin.nick@gmail.com>
2025-01-28 11:44:16 +0300

    Add logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
+#include "Application.h"
+#include "logging.h"
+
+#include <exception>
+
 int main(int argc, char* argv[])
 {
+    try
+    {
+        Application application(argc, argv);
+    }
+    catch (const std::exception& exception)
+    {
+        LOG_FATAL("%s", exception.what());
+        return 1;
+    }
+    catch (...)
+    {
+        LOG_FATAL("Unknown exception");
+        return 1;
+    }
+
     return 0;
 }
